b754dfe1ddbdbdca5aea0eb1a81d8c5698273e0b
arthur <arthur.leproust77@gmail.com>
2025-04-23 11:29:43 +0200

    Update class.py

--- class.py
+++ class.py
@@ -1,15 +1,21 @@
 import random
+
 list_id=[]
 class Error_room(Exception):
     pass
 class Error_clients(Exception):
     pass
 class room:
-    def __init__(self, type:str, capacity:int):
+    def __init__(self, Name:str):
+        self.nom = Name
+        self.type = str
+        self.capacity = 0
+    def __str__(self):
+        return f"Room {self.nom} with capacity of {self.capacite} personnes."
+    def add_room(self, type:str, capacity:int):
         self.type = type
         self.capacity = capacity
-    def __str__(self):
-        return f"Salle {self.nom} avec une capacité de {self.capacite} personnes."
+        return self.type, self.capacity
 
 class clients:
     def __init__(self, LastName:str, FirstName:str, mail:str):
@@ -18,7 +24,7 @@ class clients:
         self.mail = mail
         self.identity = self.identity()
         def __str__(self):
-            return f"Client {self.LastName} {self.FirstName} avec l'email {self.mail} et son numéro est {self.identity}."
+            return f"Client {self.LastName} {self.FirstName} with mail {self.mail} and is number is {self.identity}."
         def identity(self):
             self.identity=random.randint(1,9999)
             if self.identity in list_id:
@@ -26,6 +32,28 @@ class clients:
             else:
                 list_id.append(self.identity)
                 return self.identity
+        def add_client(self, LastName:str, FirstName:str, mail:str):
+            self.LastName = LastName
+            self.FirstName = FirstName
+            self.mail = mail
+            self.identity = self.identity()
+            if self.identity in list_id:
+                identity(self)
+            else:
+                list_id.append(self.identity)
+                return self.identity
+        def remove_client(self, LastName: str, FirstName: str,client: [clients]):
+            for i in client:
+                if i.LastName == LastName and i.FirstName == FirstName:
+                    client.remove(i)
+                    list_id.remove(i)
+                    return client
+                else:
+                    raise Error_clients("Client not found")
+            
+    
+
+
 
 
 
