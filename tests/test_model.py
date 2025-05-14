b9809d9a2aa66a8be7cfb35cf662990b6c9e3bb7
arthur <arthur.leproust77@gmail.com>
2025-05-14 11:56:55 +0200

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,4 +1,7 @@
-from Projet import projet
+from Model import database
+from Model import clients
+from Model import room
+from Model import reservation
 
-def test_projet():
+def test_model():
     pass
