bd7e603719e2578e9c4f7dd7d1f2da05fcbbde81
Yasmine Ait <yasmineait13@gmail.com>
2025-05-19 18:34:51 +0200

    Amélioration de l'interface, mise en place du fichier JSON

--- src/model/clients.py
+++ src/model/clients.py
@@ -1,15 +1,35 @@
 import uuid
 
+
 class ErrorClients(Exception):
     pass
 
+
 class Clients:
-    def __init__(self, LastName:str, FirstName:str, mail:str, password:str):
+    def __init__(self, LastName: str, FirstName: str, mail: str, password: str):
         self.password = password
         self.LastName = LastName
         self.FirstName = FirstName
         self.mail = mail
         self.identity = str(uuid.uuid4())
-        
+
+    def to_dict(self):
+        return {
+            "nom": self.LastName,
+            "prenom": self.FirstName,
+            "mail": self.mail,
+            "password": self.password,
+            "identity": self.identity,
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            LastName=data["nom"],
+            FirstName=data["prenom"],
+            mail=data["mail"],
+            password=data["password"],
+        )
+
     def __str__(self):
         return f"Client {self.LastName} {self.FirstName} with mail {self.mail} and is number is {self.identity}."
