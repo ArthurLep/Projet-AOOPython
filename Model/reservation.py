6e2a271f3cf4168b524f578d86d3b188be51bb1a
arthur <arthur.leproust77@gmail.com>
2025-05-23 14:27:04 +0200

    Update reservation.py

--- Model/reservation.py
+++ Model/reservation.py
@@ -1,15 +1,18 @@
-from Model.clients import Clients
-from Model.room import Room
 import uuid
 from datetime import date
+from Model.clients import Clients
+from Model.room import Room
 
-class Reservation:
-    def __init__(self, client:Clients, room:Room, date_reservation:date):
-        self.client = client
-        self.room = room
-        self.date_reservation = date_reservation
-        self.id = str(uuid.uuid4())
-    def __str__(self):
-        return f"Reservation {self.id} for {self.client} in {self.room} on {self.date_reservation}."
 class ErrorReservation(Exception):
     pass
+
+class Reservation:
+    def __init__(self, client: Clients, room: Room, start_date: date, end_date: date):
+        self.client = client
+        self.room = room
+        self.start_date = start_date
+        self.end_date = end_date
+        self.id = str(uuid.uuid4())
+
+    def __str__(self):
+        return f"Reservation {self.id}: {self.client} in {self.room} from {self.start_date} to {self.end_date}"
