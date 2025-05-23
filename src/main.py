fa53094334edfe98fa9db86c96c333959c5da6c1
YasmineEl <yasmineait13@gmail.com>
2025-05-23 19:35:34 +0200

    Delete src/main.py
    Je l'ai mis dans la racine du projet

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from src.view.main_view import HomeScreen
-import customtkinter as ctk
-
-if __name__ == "__main__":
-    ctk.set_appearance_mode("system")
-    ctk.set_default_color_theme("blue")
-    # Initialisation de l'application
-    app = HomeScreen()
-    app.mainloop()
