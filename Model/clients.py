6b22b945039d43b63360415938e20bd57bfc15fc
arthur <arthur.leproust77@gmail.com>
2025-05-23 14:26:26 +0200

    Update clients.py

--- Model/clients.py
+++ Model/clients.py
@@ -4,12 +4,12 @@ class ErrorClients(Exception):
     pass
 
 class Clients:
-    def __init__(self, LastName:str, FirstName:str, mail:str, password:str):
+    def __init__(self, LastName: str, FirstName: str, mail: str, password: str):
         self.password = password
         self.LastName = LastName
         self.FirstName = FirstName
         self.mail = mail
         self.identity = str(uuid.uuid4())
-        
+
     def __str__(self):
-        return f"Client {self.LastName} {self.FirstName} with mail {self.mail} and is number is {self.identity}."
+        return f"{self.LastName} {self.FirstName} ({self.mail}) - ID: {self.identity}"
