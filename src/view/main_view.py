6fbcfebc685fc724d1b19f706c4b1e88a901f1fc
arthur <arthur.leproust77@gmail.com>
2025-05-30 09:12:54 +0200

    Update main_view.py

--- src/view/main_view.py
+++ src/view/main_view.py
@@ -18,7 +18,7 @@ class MainView(ctk.CTk):
         self.database = Database()
 
         # Apparence
-        ctk.set_appearance_mode("System")
+        ctk.set_appearance_mode("dark")
         ctk.set_default_color_theme("blue")
 
         # Grille principale
